9a12f152e31bf10598d7494e127b676ce83d6502
Adam Scarborough <scarby2@gmail.com>
2020-02-19 14:52:05 -0800

    add repo list back

--- index.js
+++ index.js
@@ -280,6 +280,7 @@ async function run() {
       await exec.exec(helm, _init);
       await exec.exec('cat /root/.helm/repository/repositories.yaml')
       await exec.exec('echo $HELM_HOME')
+      await exec.exec(helm, _repo_list);
       await exec.exec(helm, args, opts);
     }
 
