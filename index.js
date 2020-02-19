edb3f280c2ccbf45228703b3f1bb155d39b6dfda
Adam Scarborough <scarby2@gmail.com>
2020-02-19 15:15:26 -0800

    explicitly specify helm home

--- index.js
+++ index.js
@@ -280,7 +280,7 @@ async function run() {
     } else {
       await exec.exec(helm, _init);
       await exec.exec('cat /root/.helm/repository/repositories.yaml')
-      await exec.exec("echo ${HELM_HOME}")
+      core.debug(process.env.HELM_HOME)
       await exec.exec(helm, _repo_list);
       await exec.exec(helm, "home");
       await exec.exec(helm, args, opts);
